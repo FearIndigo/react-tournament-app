7ba9e12ace8a2cc6d414ad028a5261e22378842f
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-18 20:22:47 +0800

    Simplify task list implementation

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,13 +5,11 @@ import { updateTaskState } from "../lib/store";
 
 export default function TaskList() {
   // We're retrieving our state from the store
-  const tasks = useSelector((state) => [...state.taskbox.tasks]);
-  const tasksInOrder = [
-    ...tasks.filter((t) => t.state === "TASK_PINNED"), 
-    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
-  ];
-  const filteredTasks = tasksInOrder.filter((t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED");
-  const archivedTasks = tasks.filter((t) => t.state === "TASK_ARCHIVED");
+  const tasks = useSelector((state) => [
+    ...state.taskbox.tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...state.taskbox.tasks.filter((t) => t.state === "TASK_INBOX"),
+    ...state.taskbox.tasks.filter((t) => t.state === "TASK_ARCHIVED"),
+  ]);
 
   const { status } = useSelector((state) => state.taskbox);
 
@@ -59,15 +57,7 @@ export default function TaskList() {
 
   return (
     <div className="list-items" data-testid="success" key={"success"}>
-      {filteredTasks.map((task) => (
-        <Task
-          key={task.id}
-          task={task}
-          onPinTask={(task) => pinTask(task)}
-          onArchiveTask={(task) => archiveTask(task)}
-        />
-      ))}
-      {archivedTasks.map((task) => (
+      {tasks.map((task) => (
         <Task
           key={task.id}
           task={task}
