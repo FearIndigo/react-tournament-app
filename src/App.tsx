b5e77618925a7c7f2cee6f15515bba5f4cdddd50
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-24 16:11:17 +0800

    Responsive styling in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import AllTeams from './components/AllTeams.tsx'
 
 function App() {
   return (
-    <div className='flex min-h-screen items-center justify-center space-x-4 bg-gradient-to-br from-violet-500 to-fuchsia-500'>
+    <div className='flex min-h-screen flex-col items-center justify-center space-y-4 bg-gradient-to-br from-violet-500 to-fuchsia-500 md:flex-row md:space-x-4 md:space-y-0'>
       <AllMembers />
       <AllTeams />
     </div>
