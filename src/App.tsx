465a772a5ec5250f7ce80efcc45b73e80b1ee9b2
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-29 21:12:30 +0800

    Add background pattern and change document container component backgrounds

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ import Trophy from './components/Trophy.tsx'
 
 function App() {
   return (
-    <div className='flex min-h-screen flex-col items-center bg-gradient-to-br from-violet-500 to-fuchsia-500'>
-      <div className='flex flex-col items-center justify-center space-y-2 px-8 py-16 text-center text-blue-100'>
+    <div className='relative flex min-h-screen flex-col items-center bg-gradient-to-br from-violet-500 to-fuchsia-500'>
+      <div
+        className='absolute left-0 top-0 h-full w-full bg-repeat opacity-[0.05]'
+        style={{ backgroundImage: 'url(/topography.svg)' }}
+      />
+
+      <div className='relative flex flex-col items-center justify-center space-y-2 px-8 py-16 text-center text-blue-100'>
         <Trophy className='h-16 w-16 select-none' />
         <h1 className='text-5xl font-bold'>Tournament App</h1>
         <span>
@@ -23,7 +28,7 @@ function App() {
         </span>
       </div>
 
-      <div className='container grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+      <div className='container relative grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
         <AllMembers />
         <AllTeams />
         <AllGames />
