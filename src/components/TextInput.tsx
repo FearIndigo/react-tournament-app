d506a8f351d1394d19e2f0aa68b50597595e5783
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-29 21:25:06 +0800

    Set input background colours

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -33,7 +33,7 @@ function TextInput({
       value={inputValue}
       placeholder={placeholder}
       onChange={handleOnChange}
-      className={`h-8 grow truncate rounded-3xl px-2 shadow-inner ring-1 ring-current ${className}`}
+      className={`h-8 grow truncate rounded-3xl bg-blue-100 px-2 shadow-inner ring-1 ring-current ${className}`}
     />
   )
 }
