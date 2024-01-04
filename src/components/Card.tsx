bf71357037356899c4c291e14596d81303690f9d
JoeMcCleery <mccleery.joseph@gmail.com>
2024-01-04 13:27:03 +0800

    Add card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react'
+import { useSlots } from '../hooks.tsx'
+
+type CardProps = {
+  className?: string
+  children?: ReactNode
+}
+
+Card.defaultProps = {
+  className: '',
+}
+
+function Card({ className, children }: CardProps) {
+  const slots = useSlots(children)
+  return (
+    <div
+      className={`flex w-full flex-col items-center justify-center rounded-3xl text-violet-800 ${className}`}
+    >
+      <div className='bg-300 flex h-10 w-full items-center justify-center rounded-3xl'>
+        {slots.header}
+      </div>
+      {slots.content}
+    </div>
+  )
+}
+
+export default Card
