ff72bcbbaedbd1f5ad6abe0e4e2378ce43e7495a
JoeMcCleery <mccleery.joseph@gmail.com>
2024-01-07 22:54:40 +0800

    Add tournaments select for stats screen

--- src/components/StatsScreen.tsx
+++ src/components/StatsScreen.tsx
@@ -7,8 +7,13 @@ import {
   useRounds,
   useTeams,
 } from '../db/hooks.ts'
+import SelectInput from './SelectInput.tsx'
+import { useMemo, useState } from 'react'
 
 function StatsScreen() {
+  const [selectedTournament, setSelectedTournament] = useState<
+    [id: string, name: string]
+  >(['', ''])
   const { result: tournaments } = useRxData<TournamentDocType>(
     'tournaments',
     (collection) =>
@@ -16,14 +21,26 @@ function StatsScreen() {
         index: ['createdAt'],
       })
   )
-  const [brackets] = useBrackets(tournaments.map((t) => t.brackets).flat())
+  const [brackets] = useBrackets(
+    tournaments.find((t) => t.id == selectedTournament[0])?.brackets ?? []
+  )
   const [rounds] = useRounds(brackets.map((b) => b.rounds).flat())
   const [games] = useGames(rounds.map((r) => r.games).flat())
   const teamStats = useGamesTeamStats(games)
   const [teams] = useTeams(Object.keys(teamStats))
 
+  const options: [id: string, name: string][] = useMemo(
+    () => tournaments.map((t) => [t.id, t.name]),
+    [tournaments]
+  )
+
   return (
     <div className='container my-4 flex flex-col justify-center px-4'>
+      <SelectInput
+        onChange={setSelectedTournament}
+        options={options}
+        value={selectedTournament[0]}
+      />
       {teams.map((t) => (
         <span key={t.id}>
           {t.id}: {teamStats[t.id].toString()}
