f00f3b0885674dc8dc995851db8dacea0e643f45
JoeMcCleery <mccleery.joseph@gmail.com>
2024-01-12 15:37:45 +0800

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
 function Footer() {
   return (
-    <footer className='relative flex w-full items-center justify-center bg-white/20 p-4 text-violet-800 backdrop-blur-sm'>
-      <span>Tournament App &copy;Your Mum 2069</span>
+    <footer className='bg-500 relative flex w-full items-center justify-center p-4 text-violet-800'>
+      <a
+        href='https://github.com/FearIndigo/react-tournament-app'
+        title='Tournament App Github'
+        target='_blank'
+        rel='noreferrer'
+        className='button bg-300'
+      >
+        View Source Code
+      </a>
     </footer>
   )
 }
